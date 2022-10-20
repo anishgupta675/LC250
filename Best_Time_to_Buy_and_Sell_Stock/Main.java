61bf5c69b7ffed4587304dc0616ebfe26df9223d
Anish Gupta <anishgupta675@gmail.com>
2022-10-20 22:15:32 +0530

    day 1 commit

---
+++ Best_Time_to_Buy_and_Sell_Stock/Main.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min_val = Integer.MAX_VALUE, max_val = 0;
+        for(int i = 0; i < prices.length; i++) {
+            if(prices[i] < min_val) min_val = prices[i];
+            else max_val = Math.max(max_val, prices[i] - min_val);
+        }
+        return max_val;
+    }
+}
---
+++ Two_Sum/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++)
+            hmap.put(nums[i], i);
+        for(int i = 0; i < nums.length; i++)
+            if(hmap.containsKey(Integer.valueOf(target - nums[i])) && i != hmap.get(Integer.valueOf(target - nums[i]))) return new int[] { i, hmap.get(Integer.valueOf(target - nums[i])) };
+        return new int[] {};
+    }
+    public static void main() throws Exception {}
+}
