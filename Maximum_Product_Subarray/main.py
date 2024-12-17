bb28548ebe7a556121cee4fdf0eb86e88758e838
Anish Gupta <anishgupta675@gmail.com>
2024-12-17 14:27:23 +0000

    Maximum Product Subarray

---
+++ Maximum_Product_Subarray/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        result, p1, p2 = nums[0], nums[0], nums[0]
+        for i in range(1, len(nums)):
+            temp = max(nums[i], p1 * nums[i], p2 * nums[i])
+            p2 = min(nums[i], p1 * nums[i], p2 * nums[i])
+            p1 = temp
+            result = max(p1, result)
+        return result
