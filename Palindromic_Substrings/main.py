9f23626700a944bc124bc9f604585e3baa6bd84b
Anish Gupta <anishgupta675@gmail.com>
2024-12-19 16:46:25 +0000

    Palindromic Substrings

---
+++ Palindromic_Substrings/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    res = 0
+    def traverse(self, s: str, l: int, r: int):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            self.res += 1
+            l -= 1
+            r += 1
+    def countSubstrings(self, s: str) -> int:
+        for i in range(len(s)):
+            self.traverse(s, i, i)
+            self.traverse(s, i, i + 1)
+        return self.res
