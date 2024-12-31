60cc6175d949f379e3c97d14af64db56c7a23524
Anish Gupta <anishgupta675@gmail.com>
2024-12-31 15:02:17 +0000

    Longest Valid Parenthesis

---
+++ Longest_Valid_Parenthesis/main.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        longest_valid_parenthesis, st = 0, []
+        st.append(-1)
+        for i in range(len(s)):
+            if s[i] == '(':
+                st.append(i)
+            else:
+                st.pop(len(st) - 1)
+                if len(st):
+                    longest_valid_parenthesis = max(longest_valid_parenthesis, i - st[-1])
+                else:
+                    st.append(i)
+        return longest_valid_parenthesis
