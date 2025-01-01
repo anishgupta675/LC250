e4f47a4bb29f7dc1ab4535a5049028cfc78ff20a
Anish Gupta <anishgupta675@gmail.com>
2025-01-01 15:35:32 +0000

    Edit Distance

---
+++ Edit_Distance/main.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        f, s = [0] * (len(word1) + 1), [0] * (len(word2) + 1)
+        for i in range(len(word2) + 1):
+            f[i] = i
+        for i in range(len(word1) + 1):
+            s[0] = i
+            for j in range(1, len(word2) + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    s[j] = f[j - 1]
+                else:
+                    s[j] = 1 + min(f[j - 1], f[j], s[j - 1])
+            f = s
+        return f[len(word2)]
