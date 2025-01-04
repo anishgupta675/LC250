639b51fd65ac228e89974e8311fea678f1f6e6ec
Anish Gupta <anishgupta675@gmail.com>
2025-01-04 13:46:05 +0000

    Best Time to Buy and Sell Stock IV

---
+++ Best_Time_to_Buy_and_Sell_Stock_IV/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        dp = [-inf] * (2 * k + 1)
+        dp[0] = 0
+        for i in range(len(prices)):
+            for j in range(0, 2 * k - 1, 2):
+                dp[j + 1] = max(dp[j + 1], dp[j] - prices[i])
+                dp[j + 2] = max(dp[j + 2], dp[j + 1] + prices[i])
+        return max(dp)
