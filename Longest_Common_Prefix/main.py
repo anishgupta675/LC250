f8ad9b543fc1e082f349287932b267e19dc68528
Anish Gupta <anishgupta675@gmail.com>
2025-01-15 17:49:28 +0000

    Longest Common Prefix

---
+++ Longest_Common_Prefix/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not len(strs):
+            return " "
+        if len(strs) == 1:
+            return strs[0]
+        strs.sort()
+        i = 0
+        while i < min(len(strs[0]), len(strs[-1])) and strs[0][i] == strs[-1][i]:
+            i += 1
+        return strs[0][:i]
