3057b28b20281bb626e0b57b3c6f0b76ae2ce54e
Anish Gupta <anishgupta675@gmail.com>
2025-01-16 16:57:46 +0000

    Valid Palindrome II

---
+++ Valid_Palindrome_II/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        mid = (len(s) >> 1) + 1
+        rev = s[-mid:][::-1]
+        if s[:mid] == rev:
+            return True
+        for i in range(mid):
+            if s[i] != rev[i]:
+                return s[i + 1:mid] == rev[i:mid - 1] or s[i:mid - 1][i + 1:]
+        return True
