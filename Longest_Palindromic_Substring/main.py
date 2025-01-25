e1d17daac5c8c5178613b103ef6e8ca452f6ed2a
Anish Gupta <anishgupta675@gmail.com>
2025-01-25 13:40:03 +0000

    Longest Palindromic Substring

---
+++ Longest_Palindromic_Substring/main.py
@@ -0,0 +1,20 @@
+class Solution:
+    res_low = 0
+    res_high = 0
+    res_len = 0
+    def _longestPalindrome(self, s: str, low: int, high: int):
+        if low < 0 or high >= len(s) or s[low] != s[high]:
+            return
+        if high - low + 1 > self.res_len:
+            self.res_low = low
+            self.res_high = high
+            self.res_len = high - low + 1
+        return self._longestPalindrome(s, low - 1, high + 1)
+    def longestPalindrome(self, s: str) -> str:
+        self.res_low = 0
+        self.res_high = 0
+        self.res_len = 0
+        for i in range(len(s)):
+            self._longestPalindrome(s, i, i)
+            self._longestPalindrome(s, i, i + 1)
+        return s[self.res_low:self.res_high + 1]
