25b3956698ed205561a6067697dc001e18699be0
Anish Gupta <anishgupta675@gmail.com>
2025-01-29 17:16:08 +0000

    Group Anagrams

---
+++ Group_Anagrams/main.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hmap = {}
+        for i in range(len(strs)):
+            word = ''.join(sorted(strs[i]))
+            if word in hmap:
+                hmap[word].append(strs[i])
+            else:
+                hmap[word] = [strs[i]]
+        res = []
+        for i in hmap.values():
+            res.append(i)
+        return res
