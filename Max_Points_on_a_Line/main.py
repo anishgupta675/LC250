54f2f10476652340d9bf0bb86aeba7ae13b27a2c
Anish Gupta <anishgupta675@gmail.com>
2025-03-03 17:36:55 +0000

    Max Points on a Line

---
+++ Max_Points_on_a_Line/main.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+class Solution(object):
+    def maxPoints(self, points):
+        def find_line(x0, y0, x1, y1):
+            if y0 == y1:
+                return 0, y0
+            if x0 == x1:
+                return x0, None
+            else:
+                slope = (y1 - y0) / (x1 - x0)
+                intercept = y0 - slope * x0
+                return slope, intercept
+        if len(points) == 1:
+            return 1
+        lines = defaultdict(lambda: set())
+        for i in range(len(points)):
+            for j in range(i + 1, len(points)):
+                x0, y0 = points[i]
+                x1, y1 = points[j]
+                line = find_line(x0, y0, x1, y1)
+                lines[line].add(i)
+                lines[line].add(j)
+        return max([len(lines[line]) for line in lines])
