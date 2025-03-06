bf6f41bd5d5061a01ed77e6fa98c2409f242d221
Anish Gupta <anishgupta675@gmail.com>
2025-03-06 18:31:33 +0000

    Number of Digit One

---
+++ Number_of_Digit_One/main.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countDigitOne(self, n: int) -> int:
+        count, i = 0, 1
+        while i <= n:
+            count += (n / (i * 10)) * i + min(max(n % (i * 10) - i + 1, 0), i)
+            i *= 10
+        return count
