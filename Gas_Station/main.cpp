14d445ff3fd43090d97b3425ceb9dad38bfdb3d9
Anish Gupta <anishgupta675@gmail.com>
2025-03-09 17:47:51 +0000

    Gas Station

---
+++ Gas_Station/main.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int gas_available = 0, gas_required = 0, starting_station = 0;
+        for(int i = 0; i < gas.size(); i++) {
+            gas_available = gas_available + gas[i] - cost[i];
+            gas_required = gas_required + gas[i] - cost[i];
+            if(gas_required < 0) {
+                starting_station = i + 1;
+                gas_required = 0;
+            }
+        }
+        return (gas_available < 0) ? -1 : starting_station;
+    }
+};
+
+int main() {}
