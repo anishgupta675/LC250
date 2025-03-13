e5c6f3eabecfe7cfc4847b7cb52703f977610a4a
Anish Gupta <anishgupta675@gmail.com>
2025-03-13 20:46:52 +0000

    Minimum Deletions to Make Character Frequences Unique

---
+++ Minimum_Deletions_to_Make_Character_Frequencies_Unique/main.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minDeletions(string s) {
+        int min_del = 0;
+        unordered_set<int> uset;
+        unordered_map<int, int> umap;
+        for(int i = 0; i < s.length(); i++) umap[s[i]]++;
+        for(auto i : umap) {
+            if(uset.find(i.second) == uset.end()) uset.insert(i.second);
+            else {
+                while(uset.find(i.second) != uset.end() && i.second) {
+                    i.second--;
+                    min_del++;
+                }
+                uset.insert(i.second);
+            }
+        }
+        return min_del;
+    }
+};
+
+int main() {}
