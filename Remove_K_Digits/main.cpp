129df1b085cc8f94d8cf3cea84cb837c9427f653
Anish Gupta <anishgupta675@gmail.com>
2025-03-16 18:03:33 +0000

    Remove K Digits

---
+++ Remove_K_Digits/main.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        int i = 0;
+        string res = "";
+        stack<char> st;
+        while(i < num.length()) {
+            while(k && !st.empty() && st.top() >= num[i]) {
+                st.pop();
+                k--;
+            }
+            st.push(num[i]);
+            i++;
+        }
+        while(k--) st.pop();
+        while(!st.empty()) {
+            res+= st.top();
+            st.pop();
+        }
+        reverse(res.begin(), res.end());
+        while(res.length() > 1 && res[0] == '0') res.erase(0, 1);
+        return (!res.compare("")) ? "0" : res;
+    }
+};
+
+int main() {}
