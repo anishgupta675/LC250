b111618d025761478d0a60ae921a5cbb5e75bd4f
Anish Gupta <anishgupta675@gmail.com>
2025-03-18 16:35:13 +0000

    Non-overlapping Intervals

---
+++ Non_overlapping_Intervals/main.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        if(intervals.size() == 1) return 0;
+        int prev = 0, curr = 1, count = intervals.size() - 1;
+        sort(intervals.begin(), intervals.end(), [](vector<int> &a, vector<int> &b) {
+            return a[1] < b[1];
+        });
+        while(curr < intervals.size()) {
+            if(intervals[prev][1] <= intervals[curr][0]) {
+                prev = curr;
+                count--;
+            }
+            curr++;
+        }
+        return count;
+    }
+};
+
+int main() {}
