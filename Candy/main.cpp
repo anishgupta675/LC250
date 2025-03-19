06809089e3957cab5b44cc1e97e66fc656fa66db
Anish Gupta <anishgupta675@gmail.com>
2025-03-19 16:10:21 +0000

    Candy

---
+++ Candy/main.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int totalCandies = ratings.size(), i = 1;
+        while(i < ratings.size()) {
+            if(ratings[i] == ratings[i - 1]) {
+                i++;
+                continue;
+            }
+            int currentPeak = 0;
+            while(i < ratings.size() && ratings[i] > ratings[i - 1]) {
+                currentPeak++;
+                totalCandies += currentPeak;
+                i++;
+            }
+            if(i == ratings.size()) return totalCandies;
+            int currentValley = 0;
+            while(i < ratings.size() && ratings[i] < ratings[i - 1]) {
+                currentValley++;
+                totalCandies += currentValley;
+                i++;
+            }
+            totalCandies -= min(currentPeak, currentValley);
+        }
+        return totalCandies;
+    }
+};
+
+int main() {}
