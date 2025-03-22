ce9a405742c5ce8b762131ad090eea142bd5ccf5
Anish Gupta <anishgupta675@gmail.com>
2025-03-22 18:21:30 +0000

    Minimum Number of Taps to Open to Water a Garden

---
+++ Minimum_Number_of_Taps_to_Open_to_Water_a_Garden/main.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minTaps(int n, vector<int>& ranges) {
+        vector<int> maxRange(n + 1, 0);
+        for(int i = 0; i < ranges.size(); i++) {
+            int left = max(0, i - ranges[i]), right = min(n, i + ranges[i]);
+            maxRange[left] = max(maxRange[left], right);
+        }
+        int end = 0, farthest = 0, taps = 0, i = 0;
+        while(end < n) {
+            while(i <= end) {
+                farthest = max(farthest, maxRange[i]);
+                i++;
+            }
+            if(farthest <= end) return -1;
+            end = farthest;
+            taps++;
+        }
+        return taps;
+    }
+};
+
+int main() {}
